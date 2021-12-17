8283d1f66b362b08f4ce35183f93408f72c2b22a
dzavi84 <milanvidanovic@yahoo.com>
2021-12-18 00:19:17 +0100

    firstcommit1

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-
-class App extends React.Component{
-constructor(props){
-   super(props);
-
-   this.state={lat: null,errorMessage:''};
-   window.navigator.geolocation.getCurrentPosition(
-    (position)=>{
-      this.setState({lat:position.coords.latitude})
-    },
-    (err)=>{
-      this.setState({errorMessage:err.message})
+        this.state = { lat: null, errorMessage: '' };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            },
+        );
     }
-    );
 
-}
-
-  render() {
-    if(this.state.errorMessage && !this.state.lat){
-      return <div>{this.state.errorMessage}</div>
-    }
-    if(!this.state.errorMessage && this.state.lat){
-      return <div>Latitude:{this.state.lat}</div>
-    }
-    return <div>Loading !</div>
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div > { this.state.errorMessage } < /div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div > Latitude: { this.state.lat } < /div>;
+        }
+        return <div > Loading now! < /div>;
     }
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
- 
+ReactDOM.render( < App / > , document.querySelector('#root'));
